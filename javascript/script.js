9a7783129b1296e1aef73c4cd92f67cca2e3344b
olakunle-remi <91887981+olakunle-remi@users.noreply.github.com>
2022-12-27 05:40:19 +0100

    javascript file add

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+var targetDate = moment('1-1-2022');
+ 
+
+var $days = $('#days'),
+ var $hours = $('#hours'),
+ var $minutes = $('#minute'),
+ var $seconds = $('#seconds');
+
+setInterval(function(){
+ //  calculate teh time remaining
+}, 10);
+
+function updateClock( remainingTime){
+//display the difference
+}
